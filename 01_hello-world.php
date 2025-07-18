3af30deb15309d134565be34e41481406c95532d
mbtions <workbymeenakshi@gmail.com>
2025-07-18 22:41:04 +0530

    program 01 in php: learing echo

--- 01_hello-world.php
+++ 01_hello-world.php
@@ -11,7 +11,12 @@
     echo "Hello world! This is the first program in php.<br>";
     echo "In this Program, we are understanding the use of <strong>echo</strong>.";
     echo "<h3>Echo</h3>";
-    echo "<p>Echo is a function in print PHP that helps print text or content on the HTML Page. The content to be printed on the webpage is enclosed in single quotes or double quotes.</p>"
+    echo "<p>Echo is a function in print PHP that helps print text or content on the HTML Page. The content to be printed on the webpage is enclosed in single quotes or double quotes.</p>";
+echo "<hr> 
+    <p>Echo example for mathematical expression <b>10+10</b>. <br>
+    The statement <strong><pre>echo 10+10;</pre></strong> will give the output: <strong><code>";
+    echo 10+10;
+    echo "</code></strong></p>";
 ?>  
 </body>
 </html>
