7a3bb0f4c4537fabc621afc40740e21ea2b407a1
mbtions <workbymeenakshi@gmail.com>
2025-07-18 22:19:37 +0530

    program 01 in php: learing echo

---
+++ 01_hello-world.php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    echo "<h1>PHP: Getting Started</h1>";
+    echo "Hello world! This is the first program in php.<br>";
+    echo "In this Program, we are understanding the use of <strong>echo</strong>.";
+    echo "<h3>Echo</h3>";
+    echo "<p>Echo is a function in print PHP that helps print text or content on the HTML Page. The content to be printed on the webpage is enclosed in single quotes or double quotes.</p>"
+?>  
+</body>
+</html>
